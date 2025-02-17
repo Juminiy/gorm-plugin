77673df4aed2ccb445971f5c0278a6d1392384e2
GitHub Action <action@github.com>
2025-02-17 11:30:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/juminiy/gormplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/juminiy/gormplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.juminiy.gormplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
